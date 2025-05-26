b4b731804b9f1e373ea5413353b7c2fcbc82384a
Ryusei-Baba <babaryusei.kw@gmail.com>
2025-05-26 21:20:33 +0900

    ✨ feat: worldとmodelの機能追加

--- simulator/launch/simulator_gazebo.launch.py
+++ simulator/launch/simulator_gazebo.launch.py
@@ -8,33 +8,15 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
-import xacro
-
 
 def generate_launch_description():
     # -------------------------------------
     # パス設定
     # -------------------------------------
     simulator_dir = get_package_share_directory('simulator')
-    world_file = os.path.join(simulator_dir, 'world', 'tsudanuma2-3.sdf')
-    xacro_file = os.path.join(simulator_dir, 'urdf', 'orne_box_mid360_gazebo.urdf.xacro')
+    world_file = os.path.join(simulator_dir, 'worlds', 'tsudanuma2-3.sdf')
+    # world_file = os.path.join(simulator_dir, 'worlds', 'orne_box_warehouse.sdf')
     param_file = os.path.join(simulator_dir, 'config', 'sim_params.yaml')
-
-    # -------------------------------------
-    # xacroからURDFを生成
-    # -------------------------------------
-    robot_description_config = xacro.process_file(xacro_file, mappings={'use_sim': 'true'})
-    robot_description = robot_description_config.toprettyxml(indent='  ')
-
-    # -------------------------------------
-    # Gazebo起動（指定ワールドで起動）
-    # -------------------------------------
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
-        ]),
-        launch_arguments=[('gz_args', f'-r -v 4 {world_file}')]
-    )
 
     # -------------------------------------
     # パラメータファイルの読み込みと分離
@@ -45,33 +27,13 @@ def generate_launch_description():
         teleop_params = full_params.get('teleop_twist_joy_node', {}).get('ros__parameters', {})
 
     # -------------------------------------
-    # Robot State Publisher ノード
+    # Gazebo起動（指定ワールドで起動）
     # -------------------------------------
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robot_description}]
-    )
-
-    # -------------------------------------
-    # Gazebo内にロボットをスポーン
-    # -------------------------------------
-    spawn_robot_node = Node(
-        package='ros_gz_sim',
-        executable='create',
-        output='screen',
-        arguments=[
-            '-string', robot_description,
-            '-name', 'orne_box',
-            '-allow_renaming', 'false',
-            '-x', '0.0',
-            '-y', '0.0',
-            '-z', '1.0',
-            '-Y', '0.0',
-            '-R', '0.0',
-            '-P', '0.0',
-        ]
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')
+        ]),
+        launch_arguments=[('gz_args', f'-r -v 4 {world_file}')]
     )
 
     # -------------------------------------
@@ -122,8 +84,6 @@ def generate_launch_description():
     # -------------------------------------
     return LaunchDescription([
         gazebo,
-        robot_state_publisher_node,
-        spawn_robot_node,
         bridge_node,
         joy_node,
         teleop_node,
